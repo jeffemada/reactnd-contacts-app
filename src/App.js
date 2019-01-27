1aacb718ea7065e929bdd9fe8c37f2469f0a13c5
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-01-26 23:32:15 -0200

    Integrated to backend API.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React, { Component } from "react";
 import ListContacts from "./ListContacts";
+import * as ContactsAPI from "./utils/ContactsAPI.js";
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: "karen",
-        name: "Karen Isgrigg",
-        handle: "karen_isgrigg",
-        avatarURL: "http://localhost:5001/karen.jpg"
-      },
-      {
-        id: "richard",
-        name: "Richard Kalehoff",
-        handle: "richardkalehoff",
-        avatarURL: "http://localhost:5001/richard.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        handle: "tylermcginnis",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState(() => ({ contacts }));
+    });
+  }
 
   removeContact = contact => {
     this.setState(currentState => ({
       //é o novo objeto (entre '{}') que representa estado
       contacts: currentState.contacts.filter(c => c.id !== contact.id)
     }));
+    ContactsAPI.remove(contact);
   };
 
   render() {
